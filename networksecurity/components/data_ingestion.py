8ddc1ab9e02a28706de24e04e7a005160eda5283
srinjoy356 <srinjoy356@gmail.com>
2026-04-23 18:21:22 +0530

    completed data ingestion

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -38,7 +38,7 @@ class DataIngestion:
 
             df=pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
-                df=df.drop(columns=["_id"],axis=1)
+                df = df.drop(columns=["_id"])
             
             df.replace({"na":np.nan},inplace=True)
             return df
